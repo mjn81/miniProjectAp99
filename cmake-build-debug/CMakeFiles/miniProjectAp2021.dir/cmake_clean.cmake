74f508f9207a218d2a8b231278f304b2c89c1a95
mjn81 <49482065+mjn81@users.noreply.github.com>
2021-05-01 01:37:18 +0430

    p init

---
+++ cmake-build-debug/CMakeFiles/miniProjectAp2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miniProjectAp2021.dir/main.cpp.obj"
+  "libminiProjectAp2021.dll.a"
+  "miniProjectAp2021.exe"
+  "miniProjectAp2021.exe.manifest"
+  "miniProjectAp2021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/miniProjectAp2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/miniProjectAp2021.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for miniProjectAp2021.
+# This may be replaced when dependencies are built.
